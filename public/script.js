d009dca0ece15324d977d30d50b6cb3d6c5eea99
Andrew <kocheshkov@alarstudios.com>
2022-02-21 20:21:53 +0100

    init

---
+++ public/script.js
@@ -0,0 +1,108 @@
+(async () => {
+  let s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  class Connector {
+    userId = null;
+    peerConnection = null;
+    signalChanel = null;
+
+    constructor({tracks = [], signalChanel, userId}) {
+      this.userId = userId;
+      this.signalChanel = signalChanel;
+      this.peerConnection = new RTCPeerConnection();
+      this.addTracks(tracks);
+    }
+
+    addTracks(tracks) {
+      if (this.peerConnection) {
+        (Array.isArray(tracks) ? tracks : [tracks]).forEach((track) => {
+          this.peerConnection.addTrack(track);
+        });
+      }
+    }
+
+    async connect() {
+      const offer = await this.peerConnection.createOffer();
+      await this.peerConnection.setLocalDescription(offer);
+      const answer = await this.signalChanel.sendOffer(offer);
+      await this.peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    }
+
+    async acceptConnect(offer) {
+      await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      const answer = await this.peerConnection.createAnswer();
+      await this.peerConnection.setLocalDescription(answer);
+      await this.signalChanel.sendAnswer(answer);
+    }
+  }
+
+  const updateConnectionsEl = () => {
+    const connectionsEl = document.getElementById('connections');
+    connectionsEl.innerHTML = Object.keys(connectors).length;
+  }
+
+  const socket = io('/');
+  const connectors = {};
+  const addConnectorToList = (connector) => {
+    connectors[connector.userId] = connector;
+    updateConnectionsEl();
+  }
+
+  const removeConnectorFromList = (connector) => {
+    delete connectors[connector.userId];
+    updateConnectionsEl();
+  }
+
+  let stream = null;
+  let tracks = [];
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
+    tracks = stream.getTracks();
+  } catch (e) {
+    console.log(e);
+  }
+
+  const getConnector = (user_id) => {
+    if (connectors[user_id]) return connectors[user_id];
+    const signalChanel = {
+      sendOffer: async (offer) => {
+        socket.emit('offer', offer, user_id);
+        return new Promise((resolve) => {
+          const func = (answer, userId) => {
+            if (userId !== user_id) return;
+            socket.off('answer');
+            resolve(answer);
+          };
+          socket.on('answer', func);
+        });
+      },
+      sendAnswer: async (answer) => {
+        socket.emit('answer', answer, user_id);
+      },
+    };
+    return new Connector({signalChanel, tracks, userId: user_id});
+  };
+
+  socket.on('user-connected', async (userId) => {
+    const connector = getConnector(userId);
+    addConnectorToList(connector);
+    await connector.connect();
+  });
+
+  socket.on('user-disconnected', async (userId) => {
+    const connector = getConnector(userId);
+    removeConnectorFromList(connector);
+  });
+
+  socket.on('offer', async (offer, user_id) => {
+    const connector = getConnector(user_id);
+    addConnectorToList(connector);
+    await connector.acceptConnect(offer);
+  });
+
+  socket.emit('join-room', ROOM_ID);
+})();
---
+++ script.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', socket.id);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', socket.id);
+    });
+
+    socket.on('offer', (offer, userId) => {
+      io.to(userId).emit('offer', offer, socket.id);
+    });
+
+    socket.on('answer', (answer, userId) => {
+      io.to(userId).emit('answer', answer, socket.id);
+    });
+  });
+});
+
+server.listen(3000)
