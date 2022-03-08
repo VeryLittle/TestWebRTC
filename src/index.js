df7db7c1869ae9ace8847b87be19ec354a73a726
Andrew <kocheshkov@alarstudios.com>
2022-03-08 21:49:15 +0100

    121

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-import {Janus} from "./lib/Janus";
 import {createVideoRoomClient} from "./lib/VideoRoom";
 
-const clientPromise = createVideoRoomClient({debug: true});
+const clientReady = createVideoRoomClient({debug: true})
+
 
 async function connect(server, roomId, displayName) {
-	const client = await clientPromise;
-	const session = await client.createSession(server);
-	const room = await session.joinRoom(roomId);
+	const client = await clientReady
+	const session = await client.createSession(server)
+	const room = await session.joinRoom(roomId)
 
-	const pub = await room.publish({publishOptions: {display: displayName}, mediaOptions: {media: {video: true}}});
-	const myVideo = makeDisplay(displayName);
-	pub.onTrackAdded(track => {
-		console.log('onTrackAdded', track)
-		myVideo.stream.addTrack(track)
-	});
-	pub.onTrackRemoved(track => {
-		console.log('onTrackRemoved', track)
-		myVideo.stream.removeTrack(track)
-	});
+	const pub = await room.publish({publishOptions: {display: displayName}, mediaOptions: {media: {video: "lowres"}}})
+	const myVideo = makeDisplay(displayName)
+	pub.onTrackAdded(track => myVideo.stream.addTrack(track))
+	pub.onTrackRemoved(track => myVideo.stream.removeTrack(track))
 
-	const subs = {};
-	room.onPublisherAdded(publishers => publishers.forEach(subscribe));
-	room.onPublisherRemoved(unsubscribe);
+	const subs = {}
+	room.onPublisherAdded(publishers => publishers.forEach(subscribe))
+	room.onPublisherRemoved(unsubscribe)
 
-	return {session, room, publisher: pub, subscribers: subs};
+	return {session, room, publisher: pub, subscribers: subs}
+
 
 	async function subscribe(publisher) {
 		const sub = subs[publisher.id] = await room.subscribe([{feed: publisher.id}])
@@ -31,28 +26,29 @@ async function connect(server, roomId, displayName) {
 		sub.onTrackAdded(track => sub.video.stream.addTrack(track))
 		sub.onTrackRemoved(track => sub.video.stream.removeTrack(track))
 	}
-
 	async function unsubscribe(publisherId) {
 		await subs[publisherId].unsubscribe()
 		subs[publisherId].video.remove()
 	}
 }
 
+
 function makeDisplay(displayName) {
 	const stream = new MediaStream()
-	const display = document.createElement('div');
-	display.classList.add('display');
-	display.innerHTML = `<div class="name">${displayName}</div><video autoplay></video>`;
-	document.querySelector('#video-grid').append(display);
-	Janus.attachMediaStream(display.querySelector('video'), stream);
+	const $display = $("<div class='display'><div class='name'></div><video autoplay></video></div>").appendTo("#displays")
+	$display.find(".name").text(displayName)
+	Janus.attachMediaStream($display.find("video").get(0), stream)
 	return {
-		stream,
-		remove: () => display.remove()
+		stream: stream,
+		remove: () => $display.remove()
 	}
 }
 
-setTimeout(() => {
-	connect('wss://farm.maindp.ru/ws', 1234, 'MyDisplayName')
-		.then(() => '')
-		.catch(console.error)
-}, 5000);
+$(function() {
+	$("#main-form").submit(function() {
+		connect(this.server.value, Number(this.roomId.value), this.displayName.value)
+			.then(() => $(this).hide())
+			.catch(console.error)
+		return false
+	})
+})
